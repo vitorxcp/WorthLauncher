416a67ddd0f44ea5df7a0312703fe4069cd94bb0
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-10 12:44:46 -0300

    Create RichPresencePlugin.js

---
+++ plugins/RichPresencePlugin.js
@@ -0,0 +1,58 @@
+const { settings } = require("./settingsRegister");
+
+const date = new Date();
+const dateNow = Date.now();
+
+let rpcInterval = null;
+let rpc = null;
+let nickname = null;
+let details = null;
+let state = null;
+
+module.exports.updateDiscordActivity = (detailsa, statea, rpca, nick) => {
+    if (rpca) {
+        rpc = rpca
+        nickname = nick;
+
+        if (detailsa) details = detailsa;
+        if (statea) state = statea;
+
+        if (!settings.discordRichPresence) {
+            rpc.clearActivity().catch((err) => {
+                console.error("[RPC] Erro ao limpar atividade:", err);
+            });
+
+            return;
+        };
+
+        if (rpcInterval) clearInterval(rpcInterval);
+
+        const activityUpdater = () => {
+            if (!rpc) return;
+            if (!settings.discordRichPresence) {
+                rpc.clearActivity().catch((err) => {
+                    console.error("[RPC] Erro ao limpar atividade:", err);
+                });
+
+                return;
+            };
+
+            rpc.setActivity({
+                details: details,
+                state: state,
+                largeImageKey: 'large_image',
+                largeImageText: 'WorthLauncher',
+                smallImageKey: `https://mc-heads.net/avatar/${nickname}/128`,
+                smallImageText: nickname,
+                instance: false,
+                startTimestamp: dateNow
+            }).catch((err) => {
+                console.error("[RPC] Erro ao atualizar:", err);
+            });
+        };
+
+        activityUpdater();
+
+        rpcInterval = setInterval(activityUpdater, 1000);
+    }
+}
