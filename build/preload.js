9e57246973ccd247378c0ba14ead3da26122f432
vitorxp <72953693+vitorxcp@users.noreply.github.com>
2025-12-17 14:53:06 -0300

    Update preload.js

--- build/preload.js
+++ build/preload.js
@@ -5,24 +5,40 @@ var version = package.version;
 
 contextBridge.exposeInMainWorld('api', {
     version,
+
+    // Auth
     loginMicrosoft: () => ipcRenderer.invoke('auth:microsoft'),
     loginOffline: (nick) => ipcRenderer.invoke('auth:offline', nick),
+
+    // Game Logic
     launchGame: (authPayload, config) => ipcRenderer.invoke('game:launch', authPayload, config),
+    abortGame: () => ipcRenderer.invoke('game:abort'),
+
+    // Window Management
     createShortcut: () => ipcRenderer.invoke('system:create-shortcut'),
     minimize: () => ipcRenderer.send('window:minimize'),
     close: () => ipcRenderer.send('window:close'),
-    abortGame: () => ipcRenderer.invoke('game:abort'),
+
+    // Texture Pack System
+    checkInstalledTextures: () => ipcRenderer.invoke('texture:check-all'),
+    installTexture: (packId) => ipcRenderer.invoke('texture:install', packId),
+
+    // Event Listeners
     onGameStarted: (cb) => {
         const listener = () => cb();
         ipcRenderer.on('game:started', listener);
         return () => ipcRenderer.removeListener('game:started', listener);
+    },
+    onPercentDownloadTxT: (cb) => {
+        const listener = (event, data) => cb(data);
+        ipcRenderer.on('outputPercentDownloadTxT', listener);
+        return () => ipcRenderer.removeListener('outputPercentDownloadTxT', listener);
     },
     onGameStartedExtra: (cb) => {
         const listener = () => cb();
         ipcRenderer.on('game:startedextra', listener);
         return () => ipcRenderer.removeListener('game:startedextra', listener);
     },
-
     onLog: (cb) => {
         const listener = (ev, msg) => cb(msg);
         ipcRenderer.on('log', listener);
@@ -38,6 +54,8 @@ contextBridge.exposeInMainWorld('api', {
         ipcRenderer.on('game:closed', listener);
         return () => ipcRenderer.removeListener('game:closed', listener);
     },
+
+    // App & User Updates
     isInstallerLaunch: () => ipcRenderer.invoke('app:check-installer-launch'),
     updateLauncher: () => ipcRenderer.send('updateVersionApp'),
     updateNickName: (nick) => ipcRenderer.invoke('user:update-nick', nick),
